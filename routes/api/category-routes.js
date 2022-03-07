70ee6d1c3e97c7d22c41971bc39f86d6e449ee8e
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-07 23:59:06 +1100

    writing category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,17 +4,65 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  }).then(cdata => res.json(cdata))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  }).then(cdata => {
+      if(!cdata){
+        res.status(404).json({message: 'This id does not match any categories.'});
+        return;
+      }else{
+        res.json(cdata);
+      }
+  }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    )
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(cdata => res.json(cdata))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
